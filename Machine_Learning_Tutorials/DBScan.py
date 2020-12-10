8fe95d01ad53e3ab98ef9a1d8aba681da4b34d39
Andrew Peterson <63623190+Andrew-C-Peterson@users.noreply.github.com>
2020-12-10 13:53:29 -0500

    ML tutorials

---
+++ Machine_Learning_Tutorials/DBScan.py
@@ -0,0 +1,83 @@
+#DBSCAN
+
+import numpy as np 
+from sklearn.cluster import DBSCAN 
+from sklearn.datasets.samples_generator import make_blobs 
+from sklearn.preprocessing import StandardScaler 
+import matplotlib.pyplot as plt 
+
+def createDataPoints(centroidLocation, numSamples, clusterDeviation):
+    # Create random data and store in feature matrix X and response vector y.
+    X, y = make_blobs(n_samples=numSamples, centers=centroidLocation, 
+                                cluster_std=clusterDeviation)
+    
+    # Standardize features by removing the mean and scaling to unit variance
+    X = StandardScaler().fit_transform(X)
+    return X, y
+
+X, y = createDataPoints([[4,3], [2,-1], [-1,4]] , 1500, 0.5)
+
+epsilon = 0.3
+minimumSamples = 7
+db = DBSCAN(eps=epsilon, min_samples=minimumSamples).fit(X)
+labels = db.labels_
+
+# Firts, create an array of booleans using the labels from db.
+core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+core_samples_mask[db.core_sample_indices_] = True
+
+# Number of clusters in labels, ignoring noise if present.
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+
+# Remove repetition in labels by turning it into a set.
+unique_labels = set(labels)
+
+# Create colors for the clusters.
+colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
+# Plot the points with colors
+for k, col in zip(unique_labels, colors):
+    if k == -1:
+        # Black used for noise.
+        col = 'k'
+
+    class_member_mask = (labels == k)
+
+    # Plot the datapoints that are clustered
+    xy = X[class_member_mask & core_samples_mask]
+    plt.scatter(xy[:, 0], xy[:, 1],s=50, c=col, marker=u'o', alpha=0.5)
+
+    # Plot the outliers
+    xy = X[class_member_mask & ~core_samples_mask]
+    plt.scatter(xy[:, 0], xy[:, 1],s=50, c=col, marker=u'o', alpha=0.5)
+    
+from sklearn.cluster import KMeans 
+k = 3
+k_means3 = KMeans(init = "k-means++", n_clusters = k, n_init = 12)
+k_means3.fit(X)
+fig = plt.figure(figsize=(6, 4))
+ax = fig.add_subplot(1, 1, 1)
+for k, col in zip(range(k), colors):
+    my_members = (k_means3.labels_ == k)
+    plt.scatter(X[my_members, 0], X[my_members, 1],  c=col, marker=u'o', alpha=0.5)
+plt.show()
+
+import csv
+import pandas as pd
+import numpy as np
+
+filename='weather-stations20140101-20141231.csv'
+
+#Read csv
+pdf = pd.read_csv(filename)
+
+pdf = pdf[pd.notnull(pdf["Tm"])]
+pdf = pdf.reset_index(drop=True)
+
+#I was having issues with visualization for the following parts, but here's
+#the code for performing the clustering
+
+# To collect data based on stations        
+
+
+
+
